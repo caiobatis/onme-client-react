2fedbc051753f26106c7965359baa3c6e25c1ea2
Caio Batista Lab <caio.batista@labotit.com.br>
2019-03-25 10:37:31 -0300

    gh pages deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89724822a5c7a64e7b8ee01d0778b28e.js"
+  "/onme-client-react/precache-manifest.b390ce871ba07bda394fb5201234e219.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/onme-client-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
