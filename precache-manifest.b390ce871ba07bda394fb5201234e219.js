56bac6fc80ad9c9f637440fe55c3d1a9e234d9c9
Caio Batista Lab <caio.batista@labotit.com.br>
2019-03-25 10:36:17 -0300

    Updates

---
+++ precache-manifest.b390ce871ba07bda394fb5201234e219.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98b17322e48d82fe7e895fdbbbff5099",
+    "url": "/onme-client-react/static/media/logo-onme-color.98b17322.png"
+  },
+  {
+    "revision": "84ad8f3bfdd878ef08b6e4605a68d614",
+    "url": "/onme-client-react/static/media/bg-header.84ad8f3b.svg"
+  },
+  {
+    "revision": "bcbf2de0a6672fc4ca17",
+    "url": "/onme-client-react/static/js/runtime~main.3e1aa215.js"
+  },
+  {
+    "revision": "fe4e438183069bd90491",
+    "url": "/onme-client-react/static/js/main.016c1c16.chunk.js"
+  },
+  {
+    "revision": "f4c3a6fc71edcb93dfb1",
+    "url": "/onme-client-react/static/js/2.780959db.chunk.js"
+  },
+  {
+    "revision": "fe4e438183069bd90491",
+    "url": "/onme-client-react/static/css/main.8be65c7f.chunk.css"
+  },
+  {
+    "revision": "f4c3a6fc71edcb93dfb1",
+    "url": "/onme-client-react/static/css/2.d18ca566.chunk.css"
+  },
+  {
+    "revision": "3d14c5189633d369725a82c7d35bd1f1",
+    "url": "/onme-client-react/index.html"
+  }
+];
