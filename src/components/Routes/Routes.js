691c75224e58afd2a4e776dfdd0812a3f3bd475b
Caio Batista Lab <caio.batista@labotit.com.br>
2019-03-25 10:59:19 -0300

    hotfix gh pages use hashrouter

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { HashRouter as Router, Route } from "react-router-dom"
 import Home from '../../pages/Home/Home'
 import Contact from '../../pages/Contact/Contact'
 
