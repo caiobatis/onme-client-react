b307668b37839a00fd1fe61e057a9ea6330cf6c7
Caio Batista <caiobatis7@gmail.com>
2019-04-09 18:53:55 -0300

    social component

---
+++ src/components/Socials/Socials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Socials = props => {
+  return (
+    <ul className="socials">
+      <li><a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/onmecambio/"><i className="fab fa-facebook"></i></a></li>
+      <li><a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/onmecambio/"><i className="fab fa-instagram"></i></a></li>
+    </ul>
+  )
+}
+export default Socials
