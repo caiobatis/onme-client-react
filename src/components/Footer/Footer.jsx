d6732a4658d225a48f2233e0214d3fe3a5cb8e52
Caio Batista <caiobatis7@gmail.com>
2019-04-09 18:53:22 -0300

    utils links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,31 +9,33 @@ export default class Footer extends Component {
       <footer id="footer">
         <div className="container footer-container">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-5">
               <img className="logo" src={logoImg} alt=""/>
               <p>A OnMe Câmbio e Travel Service veio trazer soluções financeiras para sua viagem de forma prática e segura sendo um correspondente cambial registrado junto ao Banco Central e parceira de grandes corretoras renomadas no mercado, com profissionais voltados na excelência do atendimento, praticando as melhores taxas do mercado!</p>
             </div>
-            <div className="col-md-3 offset-md-1 col-sm-4 col-xs-6 footer-links">
+            <div className="col-md-3 col-sm-4 col-xs-6 footer-links">
               <ul>
                 <li><p className="title">COMPRE POR E-MAIL</p></li>
-                <li><Link to="/">contato@onmecambio.com.br</Link></li>
+                <li><a rel="noopener noreferrer" target="_blank" href="mailto:contato@onmecambio.com.br"><p>contato@onmecambio.com.br</p></a></li>
               </ul>
               <ul>
                 <li><p className="title">COMPRE POR TELEFONE</p></li>
-                <li><Link to="/">(11) 5078-7000</Link></li>
+                <li><p>(11) 5078-7000</p></li>
               </ul>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 footer-links">
               <ul>
                 <li><p className="title">ACOMPANHE AS <br/>COTAÇÕES</p></li>
-                <li><a rel="noopener noreferrer" target="_blank" href="https://wa.me/5511992223949">(11) 99976-0000</a></li>
+                <li><a rel="noopener noreferrer" target="_blank" href="https://wa.me/5511992223949"><p>(11) 99976-0000</p></a></li>
               </ul>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 footer-links">
               <ul>
                 <li><p className="title">Produtos</p></li>
-                <li><Link to="/">Papel moeda</Link></li>
-                <li><Link to="/">Remessa</Link></li>
+                <li><Link to="/papel-moeda">Papel moeda</Link></li>
+                <li><Link to="/remessa">Remessa</Link></li>
+                <li><Link to="/delivery">Delivery</Link></li>
+                <li><Link to="/contato">Entre em contato</Link></li>
               </ul>
             </div>
             <div className="col-md-3 offset-md-9 col-xs-12">
