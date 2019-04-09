f232558cb9440f7e98737a08792f42192788b79e
Caio Batista <caiobatis7@gmail.com>
2019-04-09 18:02:55 -0300

    add in icons preheader

--- src/components/PreHeader/PreHeader.jsx
+++ src/components/PreHeader/PreHeader.jsx
@@ -7,10 +7,10 @@ const PreHeader = props => {
       <div className="container">
         <div className="flex">
           <p className="p">Compre pelo televendas</p>
-          <p className="icon"><i className="phone-icon"></i></p>
+          <p className="icon"><i className="fa fa-phone"></i></p>
           <p className="p">11 5078-7000</p>
-          <p className="icon"><i className="phone-icon"></i></p>
-          <p className="p">11 99976-0000</p>
+          <p className="icon"><i className="fab fa-whatsapp"></i></p>
+          <p className="p"><a rel="noopener noreferrer" target="_blank" href="https://wa.me/5511992223949">11 99976-0000</a></p>
         </div>
       </div>
     </div>
